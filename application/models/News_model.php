05a6448a50fe0a5841c0694148e3b89520d3a95f
Zámbó Sándor Dávid <david.a.zambo@gmail.com>
2017-04-07 22:10:26 +0200

    Tagging recording developed, next step is paginator

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -35,17 +35,17 @@
 
       $this->load->helper('url');
 
+      $title = $this->input->post('title');
+      $body = $this->input->post('body');
+      
       //split the "tags" string into words
       $tags = explode(" ",$this->input->post('tags'));
 
-      //ask the database about the highest id in the news table, because the
-      //current news' id will be the next one.
-      $last_news_id = $this->db->query('SELECT MAX(id) FROM news');
-      $current_news_id = $last_news_id-> + 1;
+      foreach ($tags as $tag) {
+        $this->db->query("INSERT INTO tags (name, news_title) VALUES ('$tag','$title')");
+      }
 
       //post news title and body to news table
-      $title = $this->input->post('title');
-      $body = $this->input->post('body');
 
       return $this->db->query("INSERT INTO news (title, body) VALUES ('$title', '$body')");
 
